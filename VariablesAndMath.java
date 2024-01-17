a9f6b68a61e9bc0290ca227ef441fdba77008830
dmohle <10038913+dmohle@users.noreply.github.com>
2024-01-16 19:01:33 -0800

    Add files via upload

---
+++ VariablesAndMath.java
@@ -0,0 +1,19 @@
+/**
+ * This program demonstrates variables and math.
+ *
+ */
+
+public class VariablesAndMath {
+    public static void main(String[] args) {
+        System.out.println("\n Welcome to Variables and Math ! \n");
+
+        // A variable is a storage location in memory with a specific type and a name.
+
+        // Declaring an integer variable named 'a' and initializing it with the value 10
+        int a = 10;
+
+        // Declaring a double variable named 'b' and initializing it with the value 5.5
+        double b = 5.5;
+
+    }
+}
